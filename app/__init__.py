3e5eb659716420c8351dc0cc669df74a2be736ed
William-AViana <willdev.viana@gmail.com>
2026-04-09 21:22:16 -0300

    update from init aplication

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,5 @@
 from flask import Flask
-from app import routes
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "hello, world!"
-
-@app.route('/books', method=['GET'])
-def get_books():
-    return {"books":[]}
+from app import routes
